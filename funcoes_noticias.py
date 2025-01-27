f45dd23331f07b013e8ceca32a7fdb2b2b49ab92
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-01-27 13:15:34 -0300

    .

--- funcoes_noticias.py
+++ funcoes_noticias.py
@@ -3,14 +3,9 @@ from bs4 import BeautifulSoup
 
 def ultima_noticias():
     url = 'https://news.google.com/rss?gl=BR&hl=pt-BR&ceid=BR:pt-419'
-    site = get(url)
-    noticias = BeautifulSoup(site.text, 'lxml')  # Usando lxml para acelerar o parsing
-    
-    # Lista para armazenar as notícias
-    ultimas_noticias = []
-    
-    for item in noticias.findAll('item')[:3]:
-        titulo = item.title.text
-        ultimas_noticias.append(titulo)
-    
-    return ultimas_noticias
+    try:
+        site = get(url)
+        noticias = BeautifulSoup(site.text, 'lxml')
+        return [item.title.text for item in noticias.findAll('item')[:3]]
+    except Exception:
+        return ['Erro ao acessar as notícias. Verifique sua conexão.']
