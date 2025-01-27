c3b1df3dd7b76f4c9e7219799777208599a922f2
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-01-27 13:15:10 -0300

    .

--- funcoes_so.py
+++ funcoes_so.py
@@ -1,24 +1,22 @@
 import os
 from datetime import datetime
 
-def verfica_hora():
+def verifica_hora():
     hora = datetime.now().strftime("%H:%M")
-    frase = f'Agora são {hora}'
-    return frase
+    return f"Agora são {hora}"
 
-# Funções de desligamento (3600 = 1 hora)
 def desligar_5_minutos():
-    os.system('shutdown /s /t 300')  # 300 segundos = 5 minutos
+    os.system('shutdown /s /t 300')
     return 'Desligamento agendado para 5 minutos.'
 
 def desligar_10_minutos():
-    os.system('shutdown /s /t 600')  # 600 segundos = 10 minutos
+    os.system('shutdown /s /t 600')
     return 'Desligamento agendado para 10 minutos.'
 
 def desligar_15_minutos():
-    os.system('shutdown /s /t 900')  # 900 segundos = 15 minutos
+    os.system('shutdown /s /t 900')
     return 'Desligamento agendado para 15 minutos.'
 
 def cancela_desligamento():
-    os.system('shutdown /a')  # Cancela o desligamento
+    os.system('shutdown /a')
     return 'O desligamento foi cancelado.'
